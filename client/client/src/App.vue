58609f2314b9f878a35db2663a137ed12771dd5a
ogqcoder <gartho77@gmail.com>
2020-10-07 23:35:03 -0400

    made it a vue app

---
+++ client/client/src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="app">
+    <Nav />
+    <div class="content">
+      <h1>Do Better For Yourself</h1>
+      <p>
+        We of get fit or go home are here to add years to your prime and help
+        you stay there until you DIE
+      </p>
+    </div>
+    <div class="image">
+      <img src="./assets/workout.jpg" alt="" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Nav from "./components/Nav.vue";
+
+export default {
+  name: "App",
+  components: {
+    Nav,
+  },
+};
+</script>
+
+<style>
+body {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+li {
+  list-style: none;
+}
+</style>
+<style scoped>
+img {
+  width: 100%;
+  height: 100%;
+  z-index: -2;
+  position: absolute;
+  left: 0;
+  top: 0;
+  object-fit: cover;
+}
+.content {
+  color: white;
+}
+</style>
---
+++ client/client/src/components/Nav.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="hello">
+    <ul class="navi">
+      <li><a href="">Home</a></li>
+      <li><a href="">plan</a></li>
+      <li><a href="">messages</a></li>
+      <span>1</span>
+      <li><a href="">Contact</a></li>
+      <li><a href="">About us</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    be: String,
+  },
+  data() {
+    return {
+      skill: "Hello",
+    };
+  },
+
+  methods: {
+    king() {
+      console.log("hello");
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.navi {
+  display: flex;
+  margin: 0;
+  height: 50px;
+  background-color: rgb(85, 73, 73);
+  color: white;
+  align-items: center;
+}
+.navi li {
+  margin: 0 10px;
+  padding: 10px;
+  align-self: center;
+}
+.navi li a {
+  color: white;
+  text-decoration: none;
+}
+.navi li:hover {
+  background: rgb(110, 52, 52);
+}
+
+span {
+  color: white;
+  background-color: red;
+  font-size: 0.5em;
+  z-index: 3;
+  transform: translate(-21px, -9px);
+}
+</style>
